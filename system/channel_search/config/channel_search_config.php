27504685455e7265e6c7813b9529d603cafeea43
Justin Kimbrell <justin@objectivehtml.com>
2014-02-04 17:15:00 -0500

    Updated default config

--- system/channel_search/config/channel_search_config.php
+++ system/channel_search/config/channel_search_config.php
@@ -20,4 +20,65 @@ $config = array(
 
 	'channel_search_save_searches' => TRUE,
 
+	/* --------------------------------------------------------
+	 *  Default Export Driver
+	 *
+	 *  The default driver used for exporting. The following
+	 *  formats are valid.
+	 *
+	 *  csv|json|xls
+	 * -------------------------------------------------------*/
+
+	'channel_search_export_driver' => 'csv',
+
+	/* --------------------------------------------------------
+	 *  .CSV Export Settings
+	 *
+	 *  If you need to configure your .CSV exports to output
+	 *  differently, update these settings.
+	 * -------------------------------------------------------*/
+
+	'channel_search_export_delimeter' => ',',
+
+	'channel_search_export_new_line' => '\n',
+
+	'channel_search_export_enclosure' => '"',
+	
+	/* --------------------------------------------------------
+	 *  Exclude Fields in .CSV
+	 *
+	 *  If you want to exlude certain fields from the .CSV
+	 *  export, you may enter than here.
+	 *  
+	 *  Sample Format:
+	 *  
+	 *  array(
+	 *		'your_field_name_1',
+	 *		'your_field_name_2'
+	 *  )
+	 * -------------------------------------------------------*/
+
+	'channel_search_export_exclude_fields' => array(
+		'site_id',
+		'pentry_id',
+		'forum_topic_id',
+		'ip_address',
+		'versioning_enabled',
+		'view_count_one',
+		'view_count_two',
+		'view_count_three',
+		'view_count_four',
+		'allow_comments',
+		'sticky',
+		'dst_enabled',
+		'year',
+		'month',
+		'day',
+		'expiration_date',
+		'comment_expiration_date',
+		'edit_date',
+		'recent_comment_date',
+		'comment_total'
+	),
+
 );
